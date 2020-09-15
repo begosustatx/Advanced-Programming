869144d18330fce8b4f3edfb133c187ff43c1f83
Marko Milić <marko.milic997@gmail.com>
2020-09-15 20:44:43 +0200

    all helper functions with eval, exec and execute

--- Assignment 1 Boa interpreter/code/part2/src/BoaInterp.hs
+++ Assignment 1 Boa interpreter/code/part2/src/BoaInterp.hs
@@ -9,7 +9,6 @@ module BoaInterp
 
 import BoaAST
 import Control.Monad
-import Data.Either (fromRight)
 
 type Env = [(VName, Value)]
 
@@ -19,13 +18,15 @@ data RunError = EBadVar VName | EBadFun FName | EBadArg String
 newtype Comp a = Comp {runComp :: Env -> (Either RunError a, [String]) }
 
 instance Monad Comp where
-  return = \a -> Comp (\_ -> (Right a,[mempty])) {-
+  return = \a -> Comp (\_ -> (Right a,[])) {-
   (>>=) = \m f -> Comp (\env -> do (a,s') <- runComp m env
                                    (b,s'') <- runComp (f a) env
                                    return [s''])-}
   m >>= f = Comp (\env -> case runComp m env of
                                (Left e, s') -> (Left e, s')
-                               (Right a, _) -> runComp (f a) env)
+                               (Right a, s') -> case runComp (f a) env of
+                                                  (Left e', s'') -> (Left e',s'<>s'')
+                                                  (Right b, s'') -> (Right b, s'<>s''))
 
 -- You shouldn't need to modify these
 instance Functor Comp where
@@ -38,11 +39,12 @@ abort :: RunError -> Comp a
 abort = \re -> Comp (\_ -> (Left re, mempty))  
 
 look :: VName -> Comp Value
--- ask Mr. Roberto for help big help like please help
-look vName = Comp (\env -> (Right ([ v | Just v <- return $ lookup vName env]!!0), mempty))
+look vName = Comp (\env -> case lookup vName env of 
+                            Nothing -> (Left (EBadVar vName), mempty)
+                            Just v  -> (Right v, mempty))
 
 withBinding :: VName -> Value -> Comp a -> Comp a
-withBinding = \vName v m -> Comp (\_ -> runComp m [(vName,v)])
+withBinding = \vName v m -> Comp (\env -> runComp m ([(vName,v)]<>env))
 
 output :: String -> Comp ()
 output = \s -> Comp (\_ -> (Right (), [s]))  
@@ -50,8 +52,12 @@ output = \s -> Comp (\_ -> (Right (), [s]))
 -- Helper functions for interpreter
 truthy :: Value -> Bool
 truthy FalseVal = False
+truthy (IntVal 0) = False
+truthy (StringVal "") = False
+truthy (ListVal []) = False
+truthy NoneVal = False
 truthy TrueVal = True
-truthy _ = error "Not a bool Value"
+truthy _ = True--error "Not a bool Value"
 
 truthy' :: Bool -> Value
 truthy' True = TrueVal
@@ -61,23 +67,33 @@ operate :: Op -> Value -> Value -> Either String Value
 operate Plus (IntVal v1) (IntVal v2) = Right (IntVal (v1 + v2))  
 operate Minus (IntVal v1) (IntVal v2) = Right (IntVal (v1 - v2))  
 operate Times (IntVal v1) (IntVal v2) = Right (IntVal (v1 * v2))  
-operate Div (IntVal v1) (IntVal v2) = Right (IntVal (v1 `div` v2))   
-operate Mod (IntVal v1) (IntVal v2) = Right (IntVal (v1 `mod` v2))  
+operate Div (IntVal v1) (IntVal v2) = if v2 == 0 then Left "Divide by Zero"
+                                      else Right (IntVal (v1 `div` v2))   
+operate Mod (IntVal v1) (IntVal v2) = if v2 == 0 then Left "Modulo by Zero"
+                                      else Right (IntVal (v1 `mod` v2))  
 operate Eq v1 v2 = Right $ truthy' (v1 == v2)
 operate Less (IntVal v1) (IntVal v2) = Right $ truthy' (v1 < v2)
 operate Greater (IntVal v1) (IntVal v2) = Right $ truthy' (v1 > v2)
 operate In v1 (ListVal v2) = Right $ truthy' (v1 `elem` v2) 
 operate _ _ _ = Left "Error :("
 
-apply :: FName -> [Value] -> Comp Value
-apply "print" [] = return (StringVal "")
-apply "print" (x:xs) = return x--(++) <$> (return x) <*> (apply "print" xs)
+convertToString :: Value -> String
+convertToString NoneVal = "None"
+convertToString TrueVal = "True"
+convertToString FalseVal = "False"
+convertToString (IntVal x) = show x
+convertToString (StringVal x) = x
+convertToString (ListVal x) = "[" ++ (unwords (map convertToString x)) ++ "]"
 
--- write conditions checks for range
-apply "range" [(IntVal n2)] = return $ ListVal (map (\x -> IntVal x) [0..(n2-1)])
-apply "range" ((IntVal n1):(IntVal n2):[]) = return $ ListVal (map (\x -> IntVal x) [n1..(n2-1)])
-apply "range" ((IntVal n1):(IntVal n2):(IntVal n3):[]) = return $ ListVal (map (\x -> IntVal x) [n1,(n3+1)..(n2-1)])
-apply _ _ = abort (EBadArg "WRONG!!!")
+apply :: FName -> [Value] -> Comp Value
+apply "print" x = do ; output(unwords(map convertToString x)); return NoneVal
+apply "range" [IntVal n2] = return $ ListVal (map (IntVal) [0..(n2-1)])
+apply "range" [(IntVal n1),(IntVal n2)] = return $ ListVal (map (\x -> IntVal x) [n1..(n2-1)])
+apply "range" [(IntVal n1),(IntVal n2),(IntVal n3)] = if n1<=n2 && n3<0 then return $ ListVal []
+                                                      else if n1>n2 && n3<0 then return $ ListVal (map (\x -> IntVal x) [n1,(n3+n1)..(n2+1)])
+                                                      else return $ ListVal (map (\x -> IntVal x) [n1,(n3+n1)..(n2-1)])
+apply "range" x = abort (EBadArg (show x))                                                      
+apply fName _ = abort (EBadFun fName)
 
 -- Main functions of interpreter
 eval :: Exp -> Comp Value
@@ -85,48 +101,74 @@ eval (Const v) = return v
 eval (Var vName) = look vName  
 eval (Oper Plus e1 e2) = do res1 <- eval e1
                             res2 <- eval e2
-                            return (fromRight (IntVal 0) (operate Plus res1 res2))
+                            case operate Plus res1 res2 of
+                                 Left s -> abort (EBadFun s)
+                                 Right v -> return v
 eval (Oper Minus e1 e2) = do res1 <- eval e1
                              res2 <- eval e2
-                             return (fromRight (IntVal 0) (operate Minus res1 res2))
+                             case operate Minus res1 res2 of
+                                 Left s -> abort (EBadFun s)
+                                 Right v -> return v
 eval (Oper Times e1 e2) = do res1 <- eval e1
                              res2 <- eval e2
-                             return (fromRight (IntVal 0) (operate Times res1 res2))
+                             case operate Times res1 res2 of
+                                 Left s -> abort (EBadFun s)
+                                 Right v -> return v
 eval (Oper Div e1 e2) = do res1 <- eval e1
                            res2 <- eval e2
-                           return (fromRight (IntVal 0) (operate Div res1 res2)) 
+                           case operate Div res1 res2 of
+                                 Left s -> abort (EBadFun s)
+                                 Right v -> return v
 eval (Oper Mod e1 e2) = do res1 <- eval e1
                            res2 <- eval e2
-                           return (fromRight (IntVal 0) (operate Mod res1 res2))  
+                           case operate Mod res1 res2 of
+                                 Left s -> abort (EBadFun s)
+                                 Right v -> return v
 eval (Oper Eq e1 e2) = do res1 <- eval e1
                           res2 <- eval e2
                           return $ truthy' (res1 == res2) 
 eval (Oper Less e1 e2) = do res1 <- eval e1
                             res2 <- eval e2
-                            return (fromRight FalseVal (operate Less res1 res2)) 
+                            case operate Less res1 res2 of
+                                 Left s -> abort (EBadFun s)
+                                 Right v -> return v
 eval (Oper Greater e1 e2) = do res1 <- eval e1
                                res2 <- eval e2
-                               return (fromRight FalseVal (operate Greater res1 res2)) 
+                               case operate Greater res1 res2 of
+                                    Left s -> abort (EBadFun s)
+                                    Right v -> return v
 eval (Oper In e1 e2) = do res1 <- eval e1
                           res2 <- eval e2
-                          return res1 -- FIX
-eval (Not e) = case e of
-                   Const NoneVal -> return TrueVal
-                   Const FalseVal -> return TrueVal
-                   Const (IntVal 0) -> return TrueVal
-                   Const (StringVal "") -> return TrueVal
-                   Const (ListVal []) -> return TrueVal
-                   otherwise -> return FalseVal
-eval (Call fName [e]) = case fName of
-                        "print" -> do res <- eval e
-                                      apply "print" [res]
-                        "range" -> do res <- eval e
-                                      apply "range" [res]
-eval (List e) =  (fmap eval e)!!0
-eval (Compr e [cc]) =   return FalseVal
+                          case operate In res1 res2 of
+                               Left s -> abort (EBadFun s)
+                               Right v -> return v 
+eval (Not e) = do r <- eval e
+                  return (truthy' (not (truthy r)))
+eval (Call fName e) = do res <- eval (List e)
+                         case res of
+                           (ListVal x) -> apply fName x
+eval (List []) = return (ListVal [])
+eval (List (x:xs)) = do res1 <- eval x
+                        res2 <- eval (List xs)
+                        case res2 of
+                          (ListVal x) -> return (ListVal (res1:x))
+eval (Compr e [cc]) = case cc of
+                          (CCFor vName exp) -> abort (EBadFun "Not implemented")
+                          (CCIf exp) -> abort (EBadFun "Not implemented")
 
 exec :: Program -> Comp ()
-exec = undefined
+exec [] = return ()
+exec (x:xs) = case x of 
+              (SDef vName exp) -> do 
+                                    res1 <- eval exp
+                                    withBinding vName res1 (exec xs)
+              (SExp exp) -> do 
+                              eval exp
+                              exec xs
 
 execute :: Program -> ([String], Maybe RunError)
-execute = undefined
+--execute [] = return ([],Nothing)
+execute p = let prog = runComp (exec p) [] in
+              case prog of
+              (Right x, s) -> (s, Nothing)
+              (Left x, s)  -> (s, Just x)
--- Assignment 1 Boa interpreter/code/part2/tests/Test.hs
+++ Assignment 1 Boa interpreter/code/part2/tests/Test.hs
@@ -21,3 +21,148 @@ tests = testGroup "Stubby tests"
      do pgm <- read <$> readFile "examples/misc.ast"
         out <- readFile "examples/misc.out"
         execute pgm @?= (lines out, Nothing)]
+
+{-
+-- Helping code
+env = [("x",NoneVal)]
+x = undefined -- Comp a :: type
+runComp x env
+
+-- abort function test
+runComp (abort (EBadVar "x")) [("x",NoneVal)] == (Left (EBadVar "x"),[])
+runComp (abort (EBadFun "sum")) [("x",NoneVal)] == (Left (EBadFun "sum"),[])
+runComp (abort (EBadArg "x")) [("x",NoneVal)] == (Left (EBadArg "x"),[])
+
+-- look function test
+runComp (look "x") [] == (Left (EBadVar "x"),[])
+runComp (look "x") [("x",NoneVal)] == (Right NoneVal,[])
+runComp (look "x") [("y",NoneVal)] == (Left (EBadVar "x"),[])
+runComp (look "x") [("x",IntVal 3)] == (Right (IntVal 3),[])
+runComp (look "x") [("x",TrueVal)] == (Right TrueVal,[])
+runComp (look "x") [("x",FalseVal)] == (Right FalseVal,[])
+runComp (look "x") [("x",StringVal "text")] == (Right (StringVal "text"),[])
+runComp (look "x") [("x",ListVal [StringVal "text"])] == (Right (ListVal [StringVal "text"]),[])
+runComp (look "x") [("x",TrueVal),("x",FalseVal),("x",IntVal 3),("x",ListVal [StringVal "text"])] == (Right TrueVal,[])
+runComp (look "x") [("y",TrueVal),("w",FalseVal),("s",IntVal 3),("x",ListVal [StringVal "text"])] == (Right (ListVal [StringVal "text"]),[])
+runComp (look "x") [("y",TrueVal),("w",FalseVal),("s",IntVal 3),("x1",ListVal [StringVal "text"])] == (Left (EBadVar "x"),[])
+
+--withBinding function test
+runComp (withBinding "x" NoneVal (look "x")) [] == (Right NoneVal,[])
+runComp (withBinding "x" (IntVal 3) (look "x")) [] == (Right (IntVal 3),[])
+runComp (withBinding "x" (TrueVal) (look "x")) [] == (Right TrueVal,[])
+runComp (withBinding "x" (FalseVal) (look "x")) [] == (Right FalseVal,[])
+runComp (withBinding "x" (StringVal "text") (look "x")) [] == (Right (StringVal "text"),[])
+runComp (withBinding "x" (ListVal [StringVal "text"]) (look "x")) [] == (Right (ListVal [StringVal "text"]),[])
+runComp (withBinding "x" (ListVal [StringVal "text",TrueVal, FalseVal]) (look "x")) [] == (Right (ListVal [StringVal "text",TrueVal, FalseVal]),[])
+
+--output function test
+output :: String -> Comp ()
+runComp (output "x") [] == (Right (),["x"])
+runComp (output "") [] == (Right (),[""])
+runComp (output "some text") [] == (Right (),["some text"])
+runComp (output "x") [("x",NoneVal)] == (Right (),["x"])
+
+--truthy
+truthy TrueVal == True
+truthy FalseVal == False
+truthy (IntVal 1) == True
+truthy (IntVal 0) == False
+truthy (StringVal "text") == True
+truthy (StringVal "") == False
+truthy (ListVal [NoneVal]) == True
+truthy (ListVal []) == False
+truthy NoneVal == False
+
+--truthy'
+truthy' True == TrueVal
+truthy' False == FalseVal
+
+--operate
+operate Plus (IntVal 0) (IntVal 0) == Right (IntVal 0)
+operate Minus (IntVal 0) (IntVal 0) == Right (IntVal 0)
+operate Times (IntVal 0) (IntVal 0) == Right (IntVal 0)
+operate Div (IntVal 0) (IntVal 0) == Left "Divide by Zero"
+operate Mod (IntVal 0) (IntVal 0) == Left "Modulo by Zero"
+operate Eq (IntVal 0) (IntVal 0) == Right TrueVal
+operate Less (IntVal 0) (IntVal 0) == Right FalseVal
+operate Greater (IntVal 0) (IntVal 0) == Right FalseVal
+operate In (IntVal 0) (ListVal []) == Right FalseVal
+operate Plus (IntVal 85) (IntVal 45) == Right (IntVal 130)
+operate Minus (IntVal 0) (IntVal 45) == Right (IntVal (-45))
+operate Times (IntVal 1) (IntVal 100) == Right (IntVal 100)
+operate Div (IntVal 0) (IntVal 5) == Right (IntVal 0)
+operate Mod (IntVal 0) (IntVal 5) == Right (IntVal 0)
+operate Eq (IntVal 0) (IntVal 5) == Right FalseVal
+operate Eq (IntVal 0) (TrueVal) == Right FalseVal
+operate Eq (StringVal "nothing") (StringVal "something") == Right FalseVal
+operate Eq (StringVal "something") (StringVal "something") == Right TrueVal
+operate Eq (ListVal [IntVal 0]) (IntVal 5) == Right FalseVal
+operate Less (IntVal 5) (IntVal 0) == Right FalseVal
+operate Greater (IntVal 5) (IntVal 0) == Right TrueVal
+operate In (IntVal 0) (ListVal [IntVal 0]) == Right TrueVal
+operate Plus (StringVal "") (IntVal 0) == Left "Error :("
+operate Minus (NoneVal) (NoneVal) == Left "Error :("
+operate Times (StringVal "") (StringVal "") == Left "Error :("
+operate Div (IntVal 0) (NoneVal) == Left "Error :("
+operate Mod (IntVal 0) (StringVal "") == Left "Error :("
+operate Eq (ListVal []) (ListVal []) == Right TrueVal
+operate Less (StringVal "") (IntVal 0) == Left "Error :("
+operate Greater (IntVal 0) (StringVal "") == Left "Error :("
+operate In (IntVal 0) (StringVal "") == Left "Error :("
+
+convertToString (ListVal [NoneVal, TrueVal, FalseVal, IntVal 30, StringVal "something"]) == "[\"None\",\"True\",\"False\",\"30\",\"something\"]"
+
+-- apply range
+runComp (apply "range" [(IntVal 10)]) [] == (Right (ListVal [IntVal 0,IntVal 1,IntVal 2,IntVal 3,IntVal 4,IntVal 5,IntVal 6,IntVal 7,IntVal 8,IntVal 9]),[])
+runComp (apply "range" [IntVal 1,IntVal 10]) [] == (Right (ListVal [IntVal 1,IntVal 2,IntVal 3,IntVal 4,IntVal 5,IntVal 6,IntVal 7,IntVal 8,IntVal 9]),[])
+runComp (apply "range" [IntVal 1,IntVal 10,IntVal 4]) [] == (Right (ListVal [IntVal 1,IntVal 5,IntVal 9]),[])
+runComp (apply "range" [IntVal 1,IntVal 10,IntVal (-4)]) [] == (Right (ListVal []),[])
+runComp (apply "range" [IntVal 10,IntVal 1,IntVal (-3)]) [] == (Right (ListVal [IntVal 10,IntVal 7,IntVal 4]),[])
+runComp (apply "range" [IntVal 10,IntVal 1,IntVal (-4)]) [] == (Right (ListVal [IntVal 10,IntVal 6,IntVal 2]),[])
+runComp (apply "range" [IntVal 10,IntVal 1]) [] == (Right (ListVal []),[])
+runComp (apply "range" [IntVal (-10)]) [] == (Right (ListVal []),[])
+runComp (eval (Call "range" [Const NoneVal])) [] == (Left (EBadArg "[NoneVal]"),[])
+runComp (eval (Call "foobar" [])) [] == (Left (EBadFun "foobar"),[])
+runComp (eval (Call "range" [])) []  == (Left (EBadArg "[]"),[])
+
+--apply print
+runComp (apply "print" []) [] == (Right NoneVal,[""])
+runComp (apply "print" [IntVal 3]) [] == (Right NoneVal,["3"])
+runComp (apply "print" [IntVal 3, NoneVal]) [] == (Right NoneVal,["3 None"])
+runComp (apply "print" [IntVal 3, StringVal "somesad", ListVal []]) [] == (Right NoneVal,["3 somesad []"])
+runComp (apply "print" [IntVal 3, StringVal "somesad", ListVal [NoneVal, NoneVal]]) [] == (Right NoneVal,["3 somesad [None None]"])
+runComp (apply "print" [IntVal 3, StringVal "somesad", ListVal [ListVal []]]) [] == (Right NoneVal,["3 somesad [[]]"])
+
+-- eval
+runComp (eval (Oper Plus (Oper Plus (Const (IntVal 3)) (Const (IntVal 4))) (Const (IntVal 3)))) [] == (Right (IntVal 10),["","","","",""])
+
+-- eval Not
+runComp (eval (Not (Var "x"))) [] == (Left (EBadVar "x"),[])
+runComp (eval (Not (Const (IntVal 0)))) [] == (Right TrueVal,[])
+runComp (eval (Not (Const (IntVal 45)))) [] == (Right FalseVal,[])
+runComp (eval (Not (Const (StringVal "")))) [] == (Right TrueVal,[])
+runComp (eval (Not (Const (StringVal "asd")))) [] == (Right FalseVal,[])
+runComp (eval (Not (Const NoneVal))) [] == (Right TrueVal,[])
+runComp (eval (Not (Var "x"))) [] == (Left (EBadVar "x"),[]) 
+-- runComp (eval (Not (ListVal []))) [] == ERROR
+
+-- eval Call
+runComp (eval (Call "range" [Const (IntVal 1), Var "x"])) [] == (Left (EBadVar "x"),[])
+runComp (eval (Call "range" [Const (IntVal 1), Var "x"])) [("x",IntVal 5)] == (Right (ListVal [IntVal 1,IntVal 2,IntVal 3,IntVal 4]),[])
+runComp (eval (Call "range" [Const (IntVal 5)])) [] == (Right (ListVal [IntVal 0,IntVal 1,IntVal 2,IntVal 3,IntVal 4]),[])
+runComp (eval (Call "range" [Const (IntVal 1), Const (IntVal 10), Const (IntVal 2)])) [] == (Right (ListVal [IntVal 1,IntVal 3,IntVal 5,IntVal 7,IntVal 9]),[])
+runComp (eval (Call "range" [Const (IntVal 1), Const (IntVal 10)])) [] == (Right (ListVal [IntVal 1,IntVal 2,IntVal 3,IntVal 4,IntVal 5,IntVal 6,IntVal 7,IntVal 8,IntVal 9]),[])
+-- eval List
+runComp (eval (List [Const NoneVal, Const (IntVal 3)])) [] ==(Right (ListVal [NoneVal,IntVal 3]),[])
+runComp (eval (List [Const NoneVal])) [] == (Right (ListVal [NoneVal]),[])
+runComp (eval (List [])) [] == (Right (ListVal []),[])
+runComp (eval (List [Const (IntVal 3),Const (IntVal 2), Const NoneVal,Const (StringVal "2")])) [] == (Right (ListVal [IntVal 3,IntVal 2,NoneVal,StringVal "2"]),[])
+
+-- exec
+runComp (exec []) [] == (Right (),[])
+runComp (exec [SDef "x" (Call "print" [(Var "x")])]) [("x",IntVal 4)] == (Right (),["4"])
+runComp (exec [SDef "x" (Call "print" [(Var "x")]), SExp (Oper Plus (Var "x") (Const (IntVal 3))), SDef "x" (Call "print" [(Var "x")])]) [("x",IntVal 4)] == 
+  
+print(x);x=(x+3);print(x)
+-}
+
