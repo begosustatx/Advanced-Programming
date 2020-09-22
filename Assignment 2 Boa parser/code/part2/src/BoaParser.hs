2c88f68098f2f2cb115a628927fbb4dcc44d12d4
Marko Milić <marko.milic997@gmail.com>
2020-09-22 12:07:49 +0200

    Grammar, tests, var, number, string defined

--- Assignment 2 Boa parser/code/part2/src/BoaParser.hs
+++ Assignment 2 Boa parser/code/part2/src/BoaParser.hs
@@ -3,21 +3,31 @@
 module BoaParser (ParseError, parseString) where
 {- Grammer
 Program ::= Stmts
-Stmts ::= Stmt | Stmt `;" Stmts
-Stmt ::= ident `=" Expr | Expr
-Expr ::= numConst | stringConst | `None" | `True" | `False"
-        | ident | Expr Oper Expr | `not" Expr | `(" Expr `)" 
-        | ident `(" Exprz `)" | `[" Exprz `]" | `[" Expr ForClause Clausez `]"
-Oper ::= `+" | `-" | `*" | `//" | `%" | `==" | `!=" 
-        | `<" | `<=" | `>" | `>=" | `in" | `not" `in"
-ForClause ::= `for" ident `in" Expr
-IfClause ::= `if" Expr
+Stmts ::= Stmt endStmt
+endStmt ::= ";" Stmts | e
+Stmt ::= ident "=" Expr | Expr
+Expr ::= "not" Expr | Term Expr'
+Expr'  ::= Oper Term Expr' | e
+Term :: = "(" Expr ")" | ident "(" Exprz ")" | "[" Exprz "]" | "[" Expr ForClause Clausez "]" | FinalExpr
+FinalExpr :: = numConst | stringConst | "None" | "True" | "False" | ident
+Oper ::= "==" | "!=" | "<" | "<=" | ">" | ">=" | "in" | "not" "in" | ArithOper
+ArithOper  ::= "+" | "-" | FactOper
+FactOper ::= "*" | "//" | "%"  
+ForClause ::= "for" ident "in" Expr
+IfClause ::= "if" Expr
 Clausez ::= e | ForClause Clausez | IfClause Clausez
 Exprz ::= e | Exprs
-Exprs ::= Expr | Expr `," Exprs
-ident ::= (see text)
-numConst ::= (see text)
-stringConst ::= (see text)
+Exprs ::= Expr | Expr "," Exprs
+ident ::= Letter Letdigs
+Letdigs ::= Letter Letdigs | Digits Letdigs | e
+Letter ::= "_" | "A" | ... | "Z" | "a" | ... | "z"
+numConst ::= "-" Digit | Digit
+Digit   ::=  "0" | NonZero Digits 
+NonZero ::= "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+Digits ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+stringConst ::= "'" Letters "'" -- and other
+Letters ::= Letter Letters
+Letter ::= "a" | ... | "Z" | e
 -}
 
 import BoaAST
@@ -28,61 +38,100 @@ import Text.ParserCombinators.ReadP
 
 type ParseError = String -- you may replace this
 
-stmts = (do stmt; return ()) <|> 
-        (do stmt; string ";"; stmts; return ())
+program = tokenize $ (do stmts; return ()) 
 
-stmt = (do ident; string "="; expr; return ();) <|> (do expr; return ())
+stmts = tokenize $ (do stmt; endStmt; return ())
 
-expr = (do numConst; return ()) <|> (do stringConst; return()) <|> 
-       (do string "None"; return ()) <|> (do string "True"; return()) <|> (do string "False"; return ()) <|> 
-       (do stringConst; return()) <|> (do ident; return()) <|>  (do expr; oper; expr; return ()) <|> 
-       (do string "not"; expr; return()) <|> (do string "("; expr; string ")"; return()) <|> 
-       (do ident; string "("; exprz; string ")"; return()) <|> (do string "["; exprz; string "]"; return()) <|> 
-       (do string "["; expr; forClause; clausez; string "]"; return())
+stmt = tokenize $ (do ident; string "="; expr; return ()) <|> (do expr; return ())
 
-oper = (do string "+"; return ()) <|> (do string "-"; return ()) <|> (do string "*"; return ())<|>
-       (do string "//"; return ()) <|> (do string "%"; return ()) <|> (do string "=="; return ())<|>
-       (do string "!="; return ()) <|> (do string "<"; return ()) <|> (do string "<="; return ())<|>
-       (do string ">"; return ()) <|> (do string ">="; return ()) <|> (do string "in"; return ())<|>
-       (do string "not"; string "in"; return ()) 
+endStmt = tokenize $ (do string ";"; stmts; return ()) <|> (do return ())
 
-forClause = do string "for"
-               ident
-               string "in"
-               expr
-               return ()
+expr = tokenize $ (do string "not"; expr; return()) <|> (do term; expr'; return ())
 
-ifClause = do string "if"
-              expr
-              return ()
+expr' = tokenize $ (do oper; term; expr'; return()) <|> (do return ())
 
-clausez = return () <|> (do forClause
-                            clausez 
-                            return ())
-                     <|>  (do ifClause
-                              clausez
-                              return ())
+term = tokenize $ (do string "("; expr; string ")"; return()) <|> (do ident; string "("; exprz; string ")"; return()) <|>
+       (do string "["; exprz; string "]"; return()) <|> (do string "["; expr; forClause; clausez; string "]"; return())
+       <|> (do finalExpr; return ())
 
+finalExpr = tokenize $ (do numConst; return ()) <|> (do stringConst; return()) <|> (do string "None"; return ()) <|>
+            (do string "True"; return()) <|> (do string "False"; return ()) <|> (do ident; return()) 
 
-exprz = return () <|> (do exprs; return())
+oper = tokenize $ (do string "=="; return ()) <|> (do string "!="; return ()) <|> (do string "<"; return ()) <|> 
+       (do string "<="; return ()) <|> (do string ">"; return ()) <|> (do string ">="; return ()) <|> 
+       (do string "in"; return ()) <|> (do string "not"; string "in"; return ())  <|> (do arithOper; return ())
 
-exprs = (do expr
-            return ()) 
-    <|> (do expr 
-            string "," 
-            exprs
-            return ())
+arithOper = tokenize $ (do string "+"; return ()) <|> (do string "-"; return ()) <|> (do factOper; return ())
 
-ident = undefined -- (see text)
-numConst = undefined -- (see text)
-stringConst = undefined -- (see text)
+factOper = tokenize $ (do string "*"; return ()) <|> (do string "//"; return ()) <|> (do string "%"; return ())
+
+forClause = tokenize $ do string "for"
+                          ident
+                          string "in"
+                          expr
+                          return ()
+
+ifClause = tokenize $  do string "if"
+                          expr
+                          return ()
+
+clausez = tokenize $ (do forClause; clausez; return ()) <|>
+                (do ifClause; clausez; return ()) <|> return ()
+
+exprz = tokenize $ (do exprs; return()) <|> return ()
+
+exprs = tokenize $ (do expr; return ()) 
+    <|> (do expr; string ","; exprs; return ())
+
+data Keyword = Keyword String
+                      deriving (Eq, Show, Read)
+boaReservedWords :: [String]
+boaReservedWords = ["None", "True", "False", "for", "if", "in", "not"]
+
+varName :: ReadP String
+varName = do
+            fc <- firstChar
+            rest <- many1 nonFirstChar
+            return (fc:rest)
+            where
+               firstChar = satisfy (\a -> isLetter a || a == '_')
+               nonFirstChar = satisfy (\a -> isDigit a || isLetter a || a == '_')
+
+ident :: ReadP (Either Keyword Exp)        
+ident = do n <- varName 
+           if n `elem` boaReservedWords then return $ Left (Keyword n)
+           else return $ Right (Var n)
+
+-- TODO represent negatives
+numConst :: ReadP Exp
+numConst = tokenize $ do
+                     first <- (do minus;digit) <|> digit
+                     case first of
+                             '0' -> return $ Const (IntVal (read [first]))
+                             _ -> do rest <- restDigits
+                                     return $ Const (IntVal (read (first:rest)))
+                     where
+                          minus = char '-'
+                          nonZero n = n `elem` ['1'..'9']
+                          digit = satisfy (\n-> nonZero n || n=='0')
+                          restDigits = many $ satisfy isDigit
+-- TODO newline? e?
+stringConst :: ReadP Exp
+stringConst = do str <- between (char' '\'') (char' '\'') (many $ stringChar)
+                 return $ Const (StringVal str)
+              where
+                  stringChar = satisfy (\a -> isLetter a || a == '\\' || a=='\n' || a=='\'' || a==' ')
 
 tokenize :: ReadP a -> ReadP a
 tokenize p = skipSpaces >> p
+-- TODO add for # comments
+
+char' :: Char -> ReadP Char 
+char' = tokenize . char 
 
 parseString :: String -> Either ParseError () -- Program
 parseString s = do
-    case filter (null . snd ) (readP_to_S stmts s) of
+    case (readP_to_S (do r<-program;eof >> return r) s) of
         [] -> Left "No valid parse"
         [(exp, remainder)] ->
             if null remainder then Right exp
--- Assignment 2 Boa parser/code/part2/tests/Test.hs
+++ Assignment 2 Boa parser/code/part2/tests/Test.hs
@@ -18,3 +18,36 @@ tests = testGroup "Minimal tests" [
     case parseString "wow!" of
       Left e -> return ()  -- any message is OK
       Right p -> assertFailure $ "Unexpected parse: " ++ show p]
+
+-- MY TESTS
+-- readP_to_S ident "x" ==[(Right (Var "x"),"")] OK
+-- readP_to_S ident "_" == [(Right (Var "_"),"")] OK
+-- readP_to_S ident "1" == [] OK
+-- readP_to_S ident "x1" == [(Right (Var "x1"),"")] OK
+-- readP_to_S ident "x_1" == [(Right (Var "x_"),"1"),(Right (Var "x_1"),"")] OK
+-- readP_to_S ident "x11" == [(Right (Var "x1"),"1"),(Right (Var "x11"),"")] OK
+-- readP_to_S ident "_1" == [(Right (Var "_1"),"")] OK
+-- readP_to_S ident "" == [] OK
+-- readP_to_S ident "None" == [(Right (Var "No"),"ne"),(Right (Var "Non"),"e"),(Left (Keyword "None"),"")] OK
+-- readP_to_S ident "True" == [(Right (Var "Tr"),"ue"),(Right (Var "Tru"),"e"),(Left (Keyword "True"),"")] OK
+-- readP_to_S ident "False" == [(Right (Var "Fa"),"lse"),(Right (Var "Fal"),"se"),(Right (Var "Fals"),"e"),(Left (Keyword "False"),"")] OK
+-- readP_to_S ident "for" == [(Right (Var "fo"),"r"),(Left (Keyword "for"),"")] OK
+-- readP_to_S ident "if" == [(Left (Keyword "if"),"")] OK
+-- readP_to_S ident "in" == [(Left (Keyword "in"),"")] OK
+-- readP_to_S ident "not" == [(Right (Var "no"),"t"),(Left (Keyword "not"),"")] OK
+
+-- readP_to_S numConst "-5" == [(Const (IntVal 5),"")] OK RETURN TYPE PROBLEM
+-- readP_to_S numConst "-05" == [(Const (IntVal 0),"5")] OK
+-- readP_to_S numConst "-50000" == [...(Const (IntVal 50000),"")] OK RETURN TYPE PROBLEM
+-- readP_to_S numConst "- 50000" == [] OK
+-- readP_to_S numConst "+10" == [] OK
+-- readP_to_S numConst "4" == [(Const (IntVal 4),"")] OK
+
+-- readP_to_S stringConst "'asdsa'" == [(Const (StringVal "asdsa"),"")] OK
+-- readP_to_S stringConst "'asdsa@" == [] OK
+-- readP_to_S stringConst "''"  == [(Const (StringVal ""),"")] OK
+-- readP_to_S stringConst "'asdss   '" == [...,(Const (StringVal "asdss   "),"")] OK
+-- readP_to_S stringConst "'asd\'sa'" == [(Const (StringVal "asd"),"sa'"),(Const (StringVal "asd'sa"),"")] OK
+-- readP_to_S stringConst "'asd\nsa'" == [(Const (StringVal "asd\nsa"),"")] OK
+-- readP_to_S stringConst "'asd\\sa'" == [(Const (StringVal "asd\\sa"),"")] OK
+-- readP_to_S stringConst "'asd//sa'" == [] OK
