6600a22cac6b36fae2a539dbbbdc84c6f30b7cad
Marko Milić <marko.milic997@gmail.com>
2020-09-24 11:44:30 +0200

    Assignment 3 init files

---
+++ Assignment 3 Analyzing InstaHub/code/part1/src/warmup.pl
@@ -0,0 +1,14 @@
+% Advanced Programming Assignment 3
+% Skeleton for warm-up part. Predicates to implement:
+
+% add(N1, N2, N)
+
+% mult(N1, N2, N)
+
+% comp(N1, N2, A)
+
+% insert(N, TI, TO)
+
+% insertlist(Ns, TI, TO)
+
+    
