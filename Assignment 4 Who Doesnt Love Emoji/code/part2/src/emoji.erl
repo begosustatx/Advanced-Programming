6a43ede5dc8a73ec97c3f0f573ba9d26c7ebad51
Marko Milić <marko.milic997@gmail.com>
2020-10-06 14:55:41 +0200

    new datastructre for initial

--- Assignment 4 Who Doesnt Love Emoji/code/part2/src/emoji.erl
+++ Assignment 4 Who Doesnt Love Emoji/code/part2/src/emoji.erl
@@ -4,18 +4,20 @@
          analytics/5, get_analytics/2, remove_analytics/3,
          stop/1, checkUnique/1, checkInitial/2]).
 
--export([search/2, deleteEmo/2]).
-
-
 -type shortcode() :: string().
 -type emoji() :: binary().
 -type analytic_fun(State) :: fun((shortcode(), State) -> State).
 
+-export([search/2]).
 
+convertToNewInitial([], NewInitial) -> NewInitial;  
+convertToNewInitial([{S,E}|T], H) ->
+    convertToNewInitial(T, [{S,E,[]}|H]).
 
 start(Initial) -> 
     Bool = checkUnique (Initial),
-    if  Bool =:= true -> {ok, spawn(fun () -> loop(Initial) end)};
+    if  Bool =:= true -> NewInitial = convertToNewInitial(Initial, []),
+                         {ok, spawn(fun () -> loop(NewInitial) end)};
         Bool =:= false -> {error, "The shortcode is not unique"}
     end.
 
@@ -31,7 +33,8 @@ delete(E, Short) ->
 lookup(E, Short) -> 
     request_reply(E,{lookup, Short}).
 
-analytics(_, _, _, _, _) -> not_implemented.
+analytics(E, Short, Fun, Label, Init) -> 
+    request_reply(E,{stop}).
 
 get_analytics(_, _) -> not_implemented.
 
@@ -39,7 +42,6 @@ remove_analytics(_, _, _) -> not_implemented.
 
 stop(E) -> 
     request_reply(E,{stop}).
-
 
 request_reply(Pid, Request) ->
     Pid ! {self(), Request},
@@ -50,12 +52,37 @@ request_reply(Pid, Request) ->
 nonblocking(Pid, Msg) ->
 	Pid ! Msg.
 
+checkUnique([])-> true;
+checkUnique([{S,_}|T]) ->
+    L = [X || {X,_} <- T, X==S],
+    if  L==[] -> checkUnique(T);
+        true -> false
+    end.
+
+checkInitial(_,[]) -> true;
+checkInitial(S1,[{S2,_,_}|T]) ->
+    if  S1==S2 -> false;
+        S1/=S2 -> checkInitial(S1,T)
+    end.
+
+deleteEmo(_,[])->[];
+deleteEmo(E1,[{S2,E2,A}|T]) ->
+    if  E1==E2 -> deleteEmo(E1,T);
+        E1/=E2 -> [{S2,E2,A}|deleteEmo(E1,T)]
+    end.
+
+search(S1,[]) -> not_found;
+search(S1,[{S2,E2,A}|T]) ->
+    if  S1==S2 -> {E2, A};
+        S1/=S2 -> search(S1,T)
+    end.
+
 loop(Initial) ->
     receive
         {From, {new, {S1,E1}}} ->
             Bool = checkInitial(S1,Initial),
             if  Bool=:= true ->
-                    {Res, NewInitial} = {ok, [{S1,E1}|Initial]},
+                    {Res, NewInitial} = {ok, [{S1,E1,[]}|Initial]},
                     From ! {self(), Res},
                     loop(NewInitial);
                 Bool =:= false ->
@@ -64,72 +91,44 @@ loop(Initial) ->
             end;
 
         {From, {alias, {S1,S2}}} ->
-            E1 = search(S1,Initial),
-            if  E1==S1 ->
+            Found = search(S1,Initial),
+            if  Found==not_found ->
                     From ! {self(),{error, "The shortcode"++S1++"doesnt exist"}},
                     loop(Initial);
-                E1/=S1 ->
+                Found/=not_found ->
                     Bool = checkInitial(S2,Initial),
                     if  Bool=:= false ->
                             From ! {self(),{error, "The shortcode"++S2++" already exist"}},
                             loop(Initial);
                         Bool=:= true ->
-                            {Res, NewInitial} = {ok, [{S2,E1}|Initial]},
+                            {E1, A} = Found,
+                            {Res, NewInitial} = {ok, [{S2,E1,A}|Initial]},
                             From ! {self(), Res},
                             loop(NewInitial)
                     end
             end;
 
         {delete, S1} ->
-            E1 = search(S1,Initial),
-            if  E1 == S1 ->
+            Found = search(S1,Initial),
+            if  Found == not_found ->
                 	loop(Initial);
-            	E1 /= S1 ->
+            	Found /= not_found ->
+                    {E1, _} = Found,
                     NewInitial= deleteEmo(E1,Initial),
                     loop(NewInitial)
             end;
 
         {From, {lookup, S1}} ->
-            E1 = search(S1,Initial),
-            if  E1 == S1 ->
+            Found = search(S1,Initial),
+            if  Found == not_found ->
                     Res = no_emoji;
-                E1 /= S1 ->
+                Found /= not_found ->
+                    {E1, _} = Found, 
                     Res = {ok, E1}
             end,
             From ! {self(), Res},
             loop(Initial);
 
-        {From, {stop}}-> stop
+        {From, {stop}}-> From ! {self(), ok}
 
-    end.
-
-checkUnique([])-> true;
-checkUnique([{S,_}|T]) ->
-    L = [X || {X,_} <- T, X==S],
-    %Bool = checkInitial({S,E},T),
-    if  L==[] -> checkUnique(T);
-        true -> false
-    end.
-
-checkInitial(_,[]) -> true;
-checkInitial(S1,[{S2,_}|T]) ->
-    if  S1==S2 -> false;
-        S1/=S2 -> checkInitial(S1,T)
-    end.
-
-deleteShort(_,[]) ->[];
-deleteShort(S1,[{S2,E2}|T]) ->
-    if  S1==S2 -> T;
-        S1/=S2 -> [{S2,E2}|deleteShort(S1,T)]
-    end.
-
-deleteEmo(_,[])->[];
-deleteEmo(E1,[{S2,E2}|T]) ->
-    if  E1==E2 -> deleteEmo(E1,T);
-        E1/=E2 -> [{S2,E2}|deleteEmo(E1,T)]
-    end.
-search(S1,[]) -> S1;
-search(S1,[{S2,E2}|T]) ->
-    if  S1==S2 -> E2;
-        S1/=S2 -> search(S1,T)
     end.
